ebe33e12a56f1c9a07759bb61bebfa727e0a3e02
David Flaig <david.flaig@hs-heilbronn.de>
2025-12-11 00:33:28 +0100

    updated gradle

--- lib/Core/Constants/config.dart
+++ lib/Core/Constants/config.dart
@@ -15,8 +15,8 @@ class AppConfig {
       "http://188.245.211.204:7000"; // https://api.campusconnects.de  |   http://localhost:6000
   String get baseFileUrl =>
       "http://188.245.211.204:7000"; // https://api.campusconnects.de  |   http://localhost:6000
-  String get chatBotServer => "http://10.0.2.2:8000/api/chatbot";
-  // String get chatBotServer => "http://188.245.211.204:7000/chat";
+  // String get chatBotServer => "http://10.0.2.2:8000/api/chatbot";
+  String get chatBotServer => "http://188.245.211.204:7000/chat";
 
   String get termsCondiftionsUrl =>
       "https://landing.campusconnects.de/terms-conditions.htm";
