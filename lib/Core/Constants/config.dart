8795a22a3f32a6dbaa1f38317f024d0951d277e2
David Flaig <david.flaig@hs-heilbronn.de>
2026-06-10 00:38:14 +0200

    adjusted it to make it locally runnable with the growth backend

--- lib/Core/Constants/config.dart
+++ lib/Core/Constants/config.dart
@@ -12,11 +12,12 @@ class AppConfig {
   AppConfig._internal();
 
   String get baseURL =>
-      "http://188.245.211.204:7000"; // https://api.campusconnects.de  |   http://localhost:6000
+      "http://localhost:7000"; // https://api.campusconnects.de  |   http://localhost:7000 | http://188.245.211.204:7000
   String get baseFileUrl =>
-      "http://188.245.211.204:7000"; // https://api.campusconnects.de  |   http://localhost:6000
-  // String get chatBotServer => "http://10.0.2.2:8000/api/chatbot";
-  String get chatBotServer => "http://188.245.211.204:7000/chat";
+      "http://localhost:7000"; // https://api.campusconnects.de  |   http://localhost:7000 | http://188.245.211.204:7000
+
+  String get chatBotServer =>
+      "http://localhost:7000"; // http://localhost:7000 | http://188.245.211.204:7000/chat
 
   String get termsCondiftionsUrl =>
       "https://landing.campusconnects.de/terms-conditions.htm";
